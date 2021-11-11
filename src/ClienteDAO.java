fd0cbd296737a18149598cb6ea2b96dd94d20bbe
NicholasPWZ <nicholcosta@hotmail.com>
2021-11-11 14:18:54 -0300

    adicionar banco de dados

--- src/ClienteDAO.java
+++ src/ClienteDAO.java
@@ -1,27 +1,55 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class ClienteDAO implements GenericDAO<Cliente, String> {
 
-    public List<Cliente> clientes;
 
+    private Connection conexao;
+    public ClienteDAO() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url ="jdbc:mysql://localhost:3306/ecommerce";
+            conexao = DriverManager.getConnection (url, "root", "password");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
     @Override
     public void adicionar(Cliente model) {
-            for(Cliente clienteAdicionado : clientes){
-                if(model.getCpf().equals(clienteAdicionado.getCpf())){
-                    throw new IllegalArgumentException("Cliente já cadastrado");
-                }
-            }
-        this.clientes.add(model);
+        try {
+            String sql = "INSERT INTO cliente(nome,cpf,email,telefone,rua,numero,bairro,complemento,cep,cidade,estado) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
+            preparedStatement.setString(1,model.getNome());
+            preparedStatement.setString(2,model.getCpf());
+            preparedStatement.setString(3,model.getContato().getEmail());
+            preparedStatement.setString(4,model.getContato().getTelefone());
+            preparedStatement.setString(5,model.getEndereco().getRua());
+            preparedStatement.setString(6,model.getEndereco().getNum());
+            preparedStatement.setString(7,model.getEndereco().getBairro());
+            preparedStatement.setString(8,model.getEndereco().getComplemento());
+            preparedStatement.setString(9,model.getEndereco().getCep());
+            preparedStatement.setString(10,model.getEndereco().getCidade());
+            preparedStatement.setString(11,model.getEndereco().getEstado());
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+    public void deletarTodos(){
+        try{
+            PreparedStatement preparedStatement = conexao.prepareStatement("Delete FROM cliente");
+            preparedStatement.execute();
+        }
 
-    @Override
-    public Cliente buscarPorId(String model) {
-            for(Cliente clientesSalvos : clientes){
-                if (model.equals(clientesSalvos.getCpf())){
-                    return clientesSalvos;
-                }
-            }
-            throw new IllegalArgumentException("Cliente não cadastrado");
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
+    @Override
+    public Cliente buscarPorId(String model) {return null;}
 }
--- test/ClienteDAOTest.java
+++ test/ClienteDAOTest.java
@@ -1,9 +1,19 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class ClienteTest {
+public class ClienteDAOTest {
+
+    private ClienteDAO clienteDAO;
+
+    @Before
+    public void iniciarDao(){clienteDAO = new ClienteDAO();}
+    @After
+    public void deletarDao(){clienteDAO.deletarTodos();}
+
     @Test
-    public void deveCriarCliente(){
+    public void deveAdicionarCliente(){
         Endereco enderecoJoao = new Endereco("Rua 2","centro","90250010","Porto Alegre","RS");
         Contato contatoJoao = new Contato("joao123@gmail.com","51999877877");
         Cliente joao = new Cliente(contatoJoao,enderecoJoao,"Joao Derly","09087862033");
@@ -11,20 +21,7 @@ public class ClienteTest {
         Assert.assertEquals(joao.getEndereco(),enderecoJoao);
         Assert.assertEquals(joao.getNome(),"Joao Derly");
         Assert.assertEquals(joao.getCpf(),"09087862033");
-
-
-    }
-    @Test
-    public void deveCriarClienteVazio(){
-       try {
-           Cliente errado = new Cliente(null, null, " ", "1233213212323");
-
-       }catch (Exception e){
-           Assert.assertEquals("[Informe um nome válido, Informe um CPF valido]", e.getMessage());
-       }
-    }
-    @Test
-    public void testeDao(){
         ClienteDAO clienteDAO = new ClienteDAO();
+        clienteDAO.adicionar(joao);
     }
 }
